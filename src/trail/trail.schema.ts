766e6169164fa29790c874b39d5c942d367dbdec
DaviMatheus <matdavimat@gmail.com>
2024-08-25 22:59:09 -0300

    [feat]#122-fxing_lint

--- src/trail/trail.schema.ts
+++ src/trail/trail.schema.ts
@@ -10,19 +10,18 @@ export const TrailSchema = new mongoose.Schema(
   { timestamps: true, collection: 'trails' },
 );
 
-// Middleware de pré-salvamento para ajustar a ordem
 TrailSchema.pre('save', async function (next) {
   const trail = this as any;
 
-  // Se a trilha já tiver uma ordem definida, não faz nada
   if (trail.order >= 0) {
     return next();
   }
 
-  // Encontrar a trilha com a maior ordem na mesma jornada
-  const lastTrail = await mongoose.model('Trail').findOne({ journey: trail.journey }).sort({ order: -1 });
+  const lastTrail = await mongoose
+    .model('Trail')
+    .findOne({ journey: trail.journey })
+    .sort({ order: -1 });
 
-  // Definir a ordem como a próxima na sequência, começando em 0
   trail.order = lastTrail ? lastTrail.order + 1 : 0;
 
   next();
