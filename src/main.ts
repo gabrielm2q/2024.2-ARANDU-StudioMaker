bc41ca2492750f2b788f4c626fa64d7c2e848d78
Gabriel Mariano da Silva <mariano.gabriel2q@gmail.com>
2024-12-07 17:52:26 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  await app.listen(configService.get('PORT'), () => {
+  await app.listen(configService.get('PORT'), '0.0.0.0', () => {
     logger.log(`Application listening on port ${configService.get('PORT')}`);
   });
 }
